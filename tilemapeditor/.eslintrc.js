ec9cbaf782a9a9c17116ba08bb1fbf679a9f602f
Quentin Tournier <qttournier@gmail.com>
2023-06-19 09:58:00 +0200

    fix: cicdé

--- tilemapeditor/.eslintrc.js
+++ tilemapeditor/.eslintrc.js
@@ -12,12 +12,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
-  "prettier/prettier": [
-    "error",
-    {
-      "endOfLine": "auto"
-    }
-  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",

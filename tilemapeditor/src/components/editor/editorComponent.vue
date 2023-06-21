5d8514aa073187310afcc86266dcdf1c9b911b8e
Lucas SAVARY <lucas.savary@sfr.fr>
2023-06-21 10:19:40 +0200

    creation ProjectSingleton

--- tilemapeditor/src/components/editor/editorComponent.vue
+++ tilemapeditor/src/components/editor/editorComponent.vue
@@ -5,15 +5,14 @@
     </div>
 </template>
 
-
 <script lang="ts">
-    //import tabsMenu from '@/components/editor/components/tabsMenu.vue'
+    import tabsMenu from '@/components/editor/components/tabsMenu.vue'
     import tabsContent from '@/components/editor/components/tabsContent.vue'
 
     export default {
         
         components: {
-            //tabsMenu,
+            tabsMenu,
             tabsContent,
         }
 

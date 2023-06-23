3ae520ade306ada6434a6ac65bb2fffe879d2e56
Lucas SAVARY <lucas.savary@sfr.fr>
2023-06-23 11:37:38 +0200

    properties height width readonly

--- tilemapeditor/src/components/editor/components/sideMenu/propertiesTab.vue
+++ tilemapeditor/src/components/editor/components/sideMenu/propertiesTab.vue
@@ -8,13 +8,9 @@ console.log(maps.value.at(selectedMap.value))
 
 const newProperties = () => {
   let inputName = document.getElementById("floatingName").value;
-  let inputHeight = document.getElementById("floatingHeight").value;
-  let inputWidth = document.getElementById("floatingWidth").value;
 
-  if(inputName != '' && inputHeight != '' && inputWidth != ''){
+  if(inputName != ''){
     maps.value.at(selectedMap.value).name = inputName
-    maps.value.at(selectedMap.value).height = inputHeight
-    maps.value.at(selectedMap.value).width = inputWidth
   }
 }
 
@@ -33,13 +29,13 @@ const newProperties = () => {
           <div class="row mb-2">
             <div class="col mx-0">
               <div class="form-floating mb-1  mx-2">
-                <input type="number" class="form-control" id="floatingHeight" placeholder="height" :value=maps.at(selectedMap).height>
+                <input type="number" class="form-control" id="floatingHeight" placeholder="height" :value=maps.at(selectedMap).height readonly>
                 <label for="floatingHeight">Height</label>
               </div>
             </div>
             <div class="col mx-0">
               <div class="form-floating mb-1  mx-2">
-                <input type="number" class="form-control" id="floatingWidth" placeholder="width"  :value=maps.at(selectedMap).width>
+                <input type="number" class="form-control" id="floatingWidth" placeholder="width"  :value=maps.at(selectedMap).width readonly>
                 <label for="floatingWidth">Width</label>
               </div>
             </div>
